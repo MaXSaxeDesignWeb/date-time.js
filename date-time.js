7d871bd76ee201569fc5ce30b4a151ec4e5f47de
MaX Falstein <MaXFalstein@users.noreply.github.com>
2018-05-24 13:43:51 +0100

    Update date-time.js

--- date-time.js
+++ date-time.js
@@ -3,9 +3,13 @@ var d = new Date();
 var year = d.getFullYear().toString();
 var m = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 var month = m[d.getMonth()];
-var space = " ";
-var date = month.concat(space.concat(year));
+var dateSpace = " ";
+var date = month.concat(dateSpace.concat(year));
 
 // time.js
+var timeSpace = " ";
+var time = hour.concat(minute).concat(second).concat(milisecond);
 
 // date-time.js functions
+var dateTimeSpace = " ";
+var dateTime = time.concat(dateTimeSpace).concat(date);
